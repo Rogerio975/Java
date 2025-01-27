d3bed048ffec07631344902449a7dab370a4f804
Rogério Alves <rogerio.alves651@gmail.com>
2025-01-27 11:16:51 -0300

    Refatora a classe Main para usar try-with-resources ao gerenciar o Scanner.

--- Projeto-Aluno/Main.java
+++ Projeto-Aluno/Main.java
@@ -1,16 +1,16 @@
+
 import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        Aluno aluno = new Aluno();
-        System.out.print("Digite o nome do aluno: ");
-        String nome = scanner.nextLine();
-        aluno.inserirNome(nome);
-        
-        System.out.println("Nome do aluno: " + aluno.recuperarNome());
-        
-        scanner.close();
+        try (Scanner scanner = new Scanner(System.in)) {
+            Aluno aluno = new Aluno();
+            System.out.print("Digite o nome do aluno: ");
+            String nome = scanner.nextLine();
+            aluno.inserirNome(nome);
+
+            System.out.println("Nome do aluno: " + aluno.recuperarNome());
+        }
     }
 }
