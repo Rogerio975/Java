40c6974ef9e16c81745fedf2349f790176acfe86
Rogério Alves <rogerio.alves651@gmail.com>
2025-01-27 10:51:15 -0300

    Adiciona a classe Aluno com métodos para inserir e recuperar nome.

---
+++ Projeto-Aluno/Aluno.java
@@ -0,0 +1,11 @@
+public class Aluno {
+    private String nome;
+
+    public void inserirNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String recuperarNome() {
+        return this.nome;
+    }
+}
