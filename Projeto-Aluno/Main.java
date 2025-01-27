06af4bc2e719c2f47255527ae99b727e2791f1a0
Rogério Alves <rogerio.alves651@gmail.com>
2025-01-27 11:42:46 -0300

    Remove o uso de try-with-resources na classe Main ao gerenciar o Scanner.

--- Projeto-Aluno/Main.java
+++ Projeto-Aluno/Main.java
@@ -4,13 +4,12 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)) {
-            Aluno aluno = new Aluno();
-            System.out.print("Digite o nome do aluno: ");
-            String nome = scanner.nextLine();
-            aluno.inserirNome(nome);
+        Scanner scanner = new Scanner(System.in);
+        Aluno aluno = new Aluno();
+        System.out.print("Digite o nome do aluno: ");
+        String nome = scanner.nextLine();
+        aluno.inserirNome(nome);
 
-            System.out.println("Nome do aluno: " + aluno.recuperarNome());
-        }
+        System.out.println("Nome do aluno: " + aluno.recuperarNome());
     }
 }
