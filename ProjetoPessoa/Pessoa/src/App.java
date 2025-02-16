ea4e8c2139dcae39e0b3f5277e20e0721d0ea128
Rogério <rogerio.alves651@gmail.com>
2025-02-15 23:22:56 -0300

    Adiciona classes App e Pessoa, configurações do projeto e documentação inicial no README.

---
+++ ProjetoPessoa/Pessoa/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        // Criando uma instância da classe Pessoa
+        Pessoa pessoa = new Pessoa("João", "Masculino", 30);
+
+        // Exibindo os dados da pessoa
+        System.out.println(pessoa);
+    }
+}
