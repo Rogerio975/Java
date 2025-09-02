d48a42118a7d60bb0f2db2f889b17508d38b78dc
Rogério Alves <rogerio.alves651@gmail.com>
2025-09-02 11:25:12 -0300

    Remove arquivos e configurações do projeto HelloWorldApp e LoginApp

--- PaginaLogin/login-app/src/test/java/MainTest.java
+++
@@ -1,9 +0,0 @@
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-public class MainTest {
-    @Test
-    public void helloWorldTest() {
-        assertEquals(1 + 1, 2);
-    }
-}
