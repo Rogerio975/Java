074e8e64f8147a3c4c5f5cdb3b3c7ffe6e4c5141
Rogério Alves <rogerio.alves651@gmail.com>
2025-09-02 10:36:40 -0300

    Projeto inserido.

---
+++ HelloWorldApp/src/main/java/com/mycompany/helloworldapp/HelloWorldApp.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.helloworldapp;
+
+/**
+ *
+ * @author E144231
+ */
+public class HelloWorldApp {
+
+    public static void main(String[] args) {
+        System.out.println("Ola, Mundo!");
+    }
+}
