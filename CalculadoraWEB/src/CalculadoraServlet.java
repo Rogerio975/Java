4423f2e9557543b9cd5200c250f102c459d1ddf8
Rogerio Alves <rogerio.alves651@gmail.com>
2025-10-28 16:37:27 -0300

    Refactor CalculadoraServlet to handle division by zero and improve code readability

--- CalculadoraWEB/src/CalculadoraServlet.java
+++ CalculadoraWEB/src/CalculadoraServlet.java
@@ -1,10 +1,13 @@
 package br.com.calculadora;
 
 import java.io.IOException;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 
 public class CalculadoraServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         double num1 = Double.parseDouble(request.getParameter("num1"));
@@ -16,7 +19,13 @@ public class CalculadoraServlet extends HttpServlet {
             case "soma": resultado = num1 + num2; break;
             case "subtracao": resultado = num1 - num2; break;
             case "multiplicacao": resultado = num1 * num2; break;
-            case "divisao": resultado = num1 / num2; break;
+            case "divisao":
+                if (num2 == 0) {
+                    resultado = Double.NaN;
+                } else {
+                    resultado = num1 / num2;
+                }
+                break;
         }
 
         request.setAttribute("resultado", resultado);
