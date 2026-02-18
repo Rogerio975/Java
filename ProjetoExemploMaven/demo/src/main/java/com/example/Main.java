023918afd6dce7d1d62b85b2713ae871fa460b61
Rogério Alves <rogerio.alves651@gmail.com>
2026-02-18 18:09:46 -0300

    Projeto criado.

--- ProjetoExemploMaven/demo/src/main/java/com/example/Main.java
+++ ProjetoExemploMaven/demo/src/main/java/com/example/Main.java
@@ -2,6 +2,9 @@ package com.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Pessoa pessoa1 = new Pessoa("João", 30);
+        System.out.println(pessoa1);
+        Pessoa pessoa2 = new Pessoa("Maria", 25);
+        System.out.println(pessoa2);
     }
 }
