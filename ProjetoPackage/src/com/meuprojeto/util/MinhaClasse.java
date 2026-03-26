2bf3a325ea1c02a0dd03ddc849410d9c357784c4
Rogerio Alves <rogerio.alves651@gmail.com>
2026-03-25 21:38:25 -0300

    Classe MinhaClasse criada em com/meuprojeto/util.

---
+++ ProjetoPackage/src/com/meuprojeto/util/MinhaClasse.java
@@ -0,0 +1,13 @@
+package com.meuprojeto.util;
+
+public class MinhaClasse {
+    public void exibirMensagem() {
+        System.out.println("MinhaClasse está funcionando!");
+        }
+
+    public static void main(String[] args) {
+        MinhaClasse minhaClasse = new MinhaClasse();
+        minhaClasse.exibirMensagem();
+    }
+
+}
