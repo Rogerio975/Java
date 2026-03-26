cfa822be29660e821f920faa3340958731582e98
Rogerio Alves <rogerio.alves651@gmail.com>
2026-03-25 21:20:48 -0300

    Frase da linha 5 alterada!

--- ProjetoPackage/src/com/exemplo/Main.java
+++ ProjetoPackage/src/com/exemplo/Main.java
@@ -2,7 +2,7 @@ package com.exemplo;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Olá, mundo!");
+        System.out.println("Package configurado com sucesso!");
     }   
 
 }
