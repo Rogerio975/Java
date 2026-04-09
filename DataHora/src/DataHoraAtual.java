7281543abd5e1ec303d36ea56d0d8cbf2e7ff781
Rogerio Alves <rogerio.alves651@gmail.com>
2026-04-09 08:46:52 -0300

    Add DataHoraAtual class to display current date and time

---
+++ DataHora/src/DataHoraAtual.java
@@ -0,0 +1,15 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+public class DataHoraAtual {
+    public static void main(String[] args) {
+        // Obtém data e hora atual
+        LocalDateTime agora = LocalDateTime.now();
+
+        // Formata a data e hora
+        DateTimeFormatter formato = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+
+        // Exibe no console
+        System.out.println("Data e Hora Atual: " + agora.format(formato));
+    }
+}
