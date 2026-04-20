e3d244bfe431f4c5bf71089bfbbe6992f6c3ec7f
Rogerio Alves <rogerio.alves651@gmail.com>
2026-04-19 23:18:06 -0300

    Add Maven Wrapper and initial project structure with Spring Boot setup

---
+++ ProjetoBancoDeDados/rba/src/main/java/br/com/rba/RbaApplication.java
@@ -0,0 +1,13 @@
+package br.com.rba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RbaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RbaApplication.class, args);
+	}
+
+}
