319abb7b0346eb04f3b691fa249f2e04f224c6d5
Rogerio Alves <rogerio.alves651@gmail.com>
2026-04-21 20:55:39 -0300

    Add application properties for database connection and server configuration; create Produto entity model

---
+++ API/api-sistema/src/main/java/com/seuprojeto/model/Produto.java
@@ -0,0 +1,26 @@
+package com.seuprojeto.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "produtos")
+@Data // O Lombok gera getters e setters automaticamente
+public class Produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    private Double preco;
+
+    private Integer estoque;
+}
