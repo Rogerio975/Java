9d3cfe2dfa3dcf6f76c85eff0e350ab310e5a973
Rogério <rogerio.alves651@gmail.com>
2026-06-11 11:07:35 -0300

    fix: Add comment to clarify random number generation range in QuickSort example

--- quicksort/QuickSortExemplo.java
+++ quicksort/QuickSortExemplo.java
@@ -2,26 +2,26 @@ import java.util.Random;
 
 public class QuickSortExemplo {
 
-    public static void quickSort(int[] vetor, int inicio, int fim) {
+    public static void quickSort(int[] vetor, int inicio, int fim) { // Método principal do Quick Sort
         if (inicio < fim) {
-            int posicaoPivo = particionar(vetor, inicio, fim);
+            int posicaoPivo = particionar(vetor, inicio, fim); // Encontra a posição do pivô
 
-            quickSort(vetor, inicio, posicaoPivo - 1);
-            quickSort(vetor, posicaoPivo + 1, fim);
+            quickSort(vetor, inicio, posicaoPivo - 1); // Ordena a parte esquerda do pivô
+            quickSort(vetor, posicaoPivo + 1, fim);  // Ordena a parte direita do pivô
         }
     }
 
-    private static int particionar(int[] vetor, int inicio, int fim) {
+    private static int particionar(int[] vetor, int inicio, int fim) { // Método para particionar o vetor
         int pivo = vetor[fim];
         int i = inicio - 1;
 
-        for (int j = inicio; j < fim; j++) {
+        for (int j = inicio; j < fim; j++) { // Percorre o vetor do início ao fim-1
             if (vetor[j] <= pivo) {
                 i++;
 
-                int temp = vetor[i];
-                vetor[i] = vetor[j];
-                vetor[j] = temp;
+                int temp = vetor[i]; // Troca os elementos
+                vetor[i] = vetor[j]; // Troca o elemento menor ou igual ao pivô para a posição correta
+                vetor[j] = temp; // Troca o elemento atual para a posição correta
             }
         }
 
@@ -33,12 +33,12 @@ public class QuickSortExemplo {
     }
 
     public static void main(String[] args) {
-        int[] numeros = new int[100];
-        Random random = new Random();
+        int[] numeros = new int[100]; // Array para armazenar os números aleatórios
+        Random random = new Random(); // Objeto para gerar números aleatórios
 
         // Gera 100 números aleatórios
         for (int i = 0; i < numeros.length; i++) {
-            numeros[i] = random.nextInt(1000);
+            numeros[i] = random.nextInt(1000); // Números entre 0 e 999
         }
 
         System.out.println("Antes da ordenação:");
